df5d1f7154d070f45d8aa808100c51ab084785e0
BLasan <benuraab@gmail.com>
2020-07-22 23:52:25 +0530

    refactor: folder structure

--- src/main/java/middleware/library/LibrarySystem/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package middleware.library.LibrarySystem.repository;
-
-import middleware.library.LibrarySystem.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByName(String name);
-}
