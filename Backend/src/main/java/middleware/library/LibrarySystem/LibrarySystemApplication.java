22a8ce93b609f1ccb2f42db2fd7b359f2dc20c88
sankhajay <sankha.rc@gmail.com>
2020-07-26 22:14:55 +0530

    backend ready to deploy in heroku

--- Backend/src/main/java/middleware/library/LibrarySystem/LibrarySystemApplication.java
+++ Backend/src/main/java/middleware/library/LibrarySystem/LibrarySystemApplication.java
@@ -2,9 +2,17 @@ package middleware.library.LibrarySystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class LibrarySystemApplication {
+	
+	    @RequestMapping
+	    @ResponseBody
+	    String home() {
+	      return "Library-System-Spring Boot-Backend";
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySystemApplication.class, args);
