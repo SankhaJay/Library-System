e19369353f0ac9e2438de885f99942baebf5854d
Sankha Jayalath <43034616+SankhaJay@users.noreply.github.com>
2020-07-26 22:26:02 +0530

    fix

--- Backend/src/main/java/middleware/library/LibrarySystem/LibrarySystemApplication.java
+++ Backend/src/main/java/middleware/library/LibrarySystem/LibrarySystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class LibrarySystemApplication {
 	
