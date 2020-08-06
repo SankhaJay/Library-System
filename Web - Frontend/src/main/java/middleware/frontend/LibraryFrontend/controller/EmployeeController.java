c5cdfd33d5a0398317855d3d176bf20bddda1c11
sankhajay <sankha.rc@gmail.com>
2020-08-06 13:14:29 +0530

    employee edit implemented

--- Web - Frontend/src/main/java/middleware/frontend/LibraryFrontend/controller/EmployeeController.java
+++ Web - Frontend/src/main/java/middleware/frontend/LibraryFrontend/controller/EmployeeController.java
@@ -8,11 +8,14 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import middleware.frontend.LibraryFrontend.entity.Employee;
 import middleware.frontend.LibraryFrontend.entity.User;
@@ -64,6 +67,33 @@ public class EmployeeController {
         restTemplate.postForObject(URL, request, Object.class);
 //        modelAndView.setViewName("/employee.jsp");
 //        return modelAndView;
+        return home();
+    }
+	
+	@RequestMapping("/edit_employee/{id}")
+    public ModelAndView editUser(@PathVariable String id) {
+        ModelAndView modelAndView = new ModelAndView();
+        String URL = "http://localhost:3000/emp/getEmployeeById/"+ id;
+        Object employee = restTemplate.getForObject(URL, Object.class);
+        System.out.println(employee);
+        modelAndView.addObject("employee", employee);
+        modelAndView.setViewName("/edit_employee.jsp");
+        return modelAndView;
+    }
+	
+	@PostMapping("/edit_employee_form")
+    public ModelAndView editUser(Employee employee) {
+//        ModelAndView modelAndView = new ModelAndView();
+        System.out.println("ID "+ employee.getEmp_id());
+        String URL = "http://localhost:3000/emp/update";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Object> request = new HttpEntity<Object>(employee, headers);
+        restTemplate.put(URL, request, Object.class);
+//        modelAndView.setViewName("/edit_user.jsp");
+//        RedirectView redirectView = new RedirectView();
+//        redirectView.setUrl("http://localhost:9090/user");
+//        return redirectView;
         return home();
     }
 }
