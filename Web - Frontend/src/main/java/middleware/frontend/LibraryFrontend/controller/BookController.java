6e0d3a4809ad1c1696c2687b73e86e9a9b61535d
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-08-14 21:40:15 +0530

    can post book

--- Web - Frontend/src/main/java/middleware/frontend/LibraryFrontend/controller/BookController.java
+++ Web - Frontend/src/main/java/middleware/frontend/LibraryFrontend/controller/BookController.java
@@ -1,10 +1,19 @@
 package middleware.frontend.LibraryFrontend.controller;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import middleware.frontend.LibraryFrontend.entity.Book;
+import middleware.frontend.LibraryFrontend.entity.User;
 
 @RestController
 public class BookController {
@@ -20,5 +29,26 @@ public class BookController {
 	        modelAndView.setViewName("/book.jsp");
 	        return modelAndView;
 	    }
+	 
+	 @RequestMapping("/add-book")
+	    public ModelAndView userAdd() {
+	        ModelAndView modelAndView = new ModelAndView();
+	        modelAndView.setViewName("/book_add.jsp");
+	        return modelAndView;
+	    }
+	 
+	    @PostMapping("/create_book")
+	    public ModelAndView createUser(Book book) {
+	        ModelAndView modelAndView = new ModelAndView();
+	        System.out.println(book.getTitle());
+	        String URL = "http://localhost:3000/book/addBook";
+	        HttpHeaders headers = new HttpHeaders();
+	        HttpEntity<Object> request = new HttpEntity<Object>(book, headers);
+	        headers.setContentType(MediaType.APPLICATION_JSON);
+	        restTemplate.postForObject(URL, request, Object.class);
+	        modelAndView.setViewName("/book_add.jsp");
+	        return modelAndView;
+	    }
+
 	
 }
